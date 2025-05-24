20877689f530f81447e53deecd7c9715b8ce1936
Gabriel <gabrielbrachobf@gmail.com>
2025-05-24 13:34:28 -0400

    generate html whit javascript

---
+++ scripts/ferreteria.js
@@ -0,0 +1,84 @@
+const products = [{
+  imagen: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  nombre: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    estrellas: 4.5,
+    contador: 87
+  },
+  precioCents: 1090
+}, {
+  imagen: 'images/products/intermediate-composite-basketball.jpg',
+  nombre: 'Intermediate Size Basketball',
+  rating: {
+    estrellas: 4,
+    contador: 127
+  },
+  precioCents: 2095
+}, {
+  imagen: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  nombre: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    estrellas: 4.5,
+    contador: 56,
+  },
+  precioCents: 799
+}];
+
+let productsHTML = '';
+
+products.forEach((product)=> {
+  productsHTML += `
+    <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.imagen}">
+            </div>
+
+            <div class="product-name limit-text-to-2-lines">
+              ${product.nombre}
+            </div>
+
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.estrellas * 10}.png">
+              <div class="product-rating-count link-primary">
+                ${product.rating.contador}
+              </div>
+            </div>
+
+            <div class="product-price">
+              $${(product.precioCents / 100).toFixed(2)}
+            </div>
+
+            <div class="product-quantity-container">
+              <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
+
+            <button class="add-to-cart-button button-primary">
+              Add to Cart
+            </button>
+          </div>
+  `;
+});
+
+console.log(productsHTML);
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
