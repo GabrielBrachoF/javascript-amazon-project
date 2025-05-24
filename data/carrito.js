e1e07a2036b46c388646a8b843becbf6d4bada5a
Gabriel <gabrielbrachobf@gmail.com>
2025-05-24 15:01:49 -0400

    Mover agregar el carrito a carrito.js

--- data/carrito.js
+++ data/carrito.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+
+export function agregarAlCarrito(productId){
+  let matchingItem;
+
+      cart.forEach((carritoItem) => {
+        if (productId === carritoItem.productId){
+          matchingItem = carritoItem;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+}
--- scripts/ferreteria.js
+++ scripts/ferreteria.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/carrito.js';
+import { cart, agregarAlCarrito } from '../data/carrito.js';
 import { products } from '../data/products.js';
 
 let productsHTML = '';
@@ -61,34 +61,23 @@ console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
 
-      let matchingItem;
+function cantidadCarrito(productId){
+   let cartQuantity = 0;
 
-      cart.forEach((item) => {
-        if (productId === item.productId){
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-        productId: productId,
-        quantity: 1
-      });
-    }
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
+      cart.forEach((carritoItem) => {
+        cartQuantity += carritoItem.quantity;
       });
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
-    });
+}
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      agregarAlCarrito(productId);
+      cantidadCarrito(productId);
+     });
   });
