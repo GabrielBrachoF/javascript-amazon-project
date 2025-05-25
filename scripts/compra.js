b9debd1afee9cd55bcfa203f42cf5a54ed7ddb23
Gabriel <gabrielbrachobf@gmail.com>
2025-05-24 21:44:26 -0400

    eliminar productos de html

--- scripts/compra.js
+++ scripts/compra.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem) => {
   console.log(matchingProduct);
 
   cartSummaryHTML +=`
-    <div class="cart-item-container">
+    <div class="cart-item-container
+    js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -105,7 +106,11 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       eliminarDelCarro(productId);
-      console.log(cart);
+      
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
     });
   });
 
